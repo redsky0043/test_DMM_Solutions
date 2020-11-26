63857c7d6d944233b3c36e688e9ce0438eaf1b8f
Serhii Boiko <boyko0043@gmail.com>
2020-11-26 10:36:19 +0200

    Updates

---
+++ precache-manifest.827f6c78e3dc48ad62f8cb7e20e926b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef521fc73744f0d5c829f168de98f47",
+    "url": "/test_Test/index.html"
+  },
+  {
+    "revision": "82276fbdc3c51953fda3",
+    "url": "/test_Test/static/css/main.0b8fb580.chunk.css"
+  },
+  {
+    "revision": "c67e775b9abeab3a5af4",
+    "url": "/test_Test/static/js/2.edc1d0c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_Test/static/js/2.edc1d0c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82276fbdc3c51953fda3",
+    "url": "/test_Test/static/js/main.5fabc046.chunk.js"
+  },
+  {
+    "revision": "a080a4725f03305e8946",
+    "url": "/test_Test/static/js/runtime-main.9697079e.js"
+  },
+  {
+    "revision": "6092516ea927337ac66e6f3000a87d4e",
+    "url": "/test_Test/static/media/сlose.6092516e.svg"
+  }
+]);
