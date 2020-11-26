a2c03ad4cc6305fa3ed0d71f3d1e71f6c2bc7028
Serhii Boiko <boyko0043@gmail.com>
2020-11-26 10:27:50 +0200

    v1

--- src/pages/Photos/usePhotos.js
+++ src/pages/Photos/usePhotos.js
@@ -5,21 +5,17 @@ export const usePhotos = () => {
     const [dataNext, setDataNext] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState(false);
-    const [id, setId] = useState(1);
 
     useEffect(() => {
         const getPhotos = async () => {
             try {
                 setIsLoading(true);
                 const response = await fetch('https://tzfrontend.herokuapp.com/images/');
-                const responseNext = await fetch(`https://tzfrontend.herokuapp.com/images/${setId}/`);
                 const results = await response.json();
-                const resultsNext = await responseNext.json();
                 // if (error) {
                 //     throw new Error(error);
                 // }
                 setData(results);
-                setDataNext(responseNext);
                 setIsError(false);
             } catch (e) {
                 setIsError(true);
@@ -32,7 +28,6 @@ export const usePhotos = () => {
 
     return {
         data,
-        dataNext,
         isLoading,
         isError,
     };
